e286abe11ce917d67d8f6ea338d80dce0abc51ea
Ed <silva.email@yahoo.com>
2024-02-19 13:03:17 -0500

    Add Card component and update App component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
 //https://c6fkx.csb.app/
@@ -11,3 +5,15 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //3. Use props to render the default Beyonce contact card
 //so the Card component can be reused for other contacts.
 //4. Import the contacts.js file to create card components.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+import contacts from "./contacts.js";
+
+
+
+ReactDOM.render(<App  />
+,document.getElementById("root"));
+
+
