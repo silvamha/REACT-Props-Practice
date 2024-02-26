90e59d9450b355f56bc20fb2840f67289198b3c2
Ed <silva.email@yahoo.com>
2024-02-26 15:18:02 -0500

    Refactor contact population in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,23 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
-const populateContacts = contacts.map((contact) => {
-  return (
-    <Card
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
-    />
-  );
-});
-
-return (
-  <div>
-    <h1 className="heading">My Contacts</h1>
-    {populateContacts}
-  </div>
-);
-
 
 function App() {
+  const populateContacts = contacts.map((contact) => {
+    return (
+      <Card
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+      />
+    );
+  });
+
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-<Card 
-
-/>
-    {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
+      {populateContacts}
     </div>
   );
 }
